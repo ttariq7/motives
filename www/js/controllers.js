c39d0c3aaa72472964720263e89a381ebc04ee88
tariq7 <t.tariq@sellanycar.com>
2015-12-05 23:08:55 +0000

    pushing lateste

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,16 +9,19 @@ angular.module('starter.controllers', [])
     latitude:"-0.5530055"
   }
 
+  var clientId = "R3FGZMI124B1GQBTLAS45EZ5YC0TPT3VI0ZANLTUEROIORER";
+  var clientSecret = "2NM0Z15Z3NYGDGVQSFT2FOUDQZ4PPMB4L3GH3Y4HJKJALHBV";
 
-$http({
-  method: 'GET',
-  url: 'https://api.foursquare.com/v2/venues/search?client_id=R3FGZMI124B1GQBTLAS45EZ5YC0TPT3VI0ZANLTUEROIORER&client_secret=2NM0Z15Z3NYGDGVQSFT2FOUDQZ4PPMB4L3GH3Y4HJKJALHBV&v=20130815&ll=51.499840,-0.5530055&query=',
-}).then(function successCallback(response) {
+
+  $http({
+  		method: 'GET',
+  		url: 'https://api.foursquare.com/v2/venues/search?client_id='+clientId+'&client_secret='+clientSecret+'&v=20130815&ll='+location.longitude+','+location.latitude+'&query=',
+  	}).then(function successCallback(response) {
         console.log(response.data.response.venues)
         $scope.venues=response.data.response.venues;
-  }, function errorCallback(response) {
-    console.log(response)
-  });
+  	}, function errorCallback(response) {
+    	console.log(response)
+  	});
 
 
 
@@ -26,7 +29,7 @@ $http({
   function getVenues(search){
     $http({
         method: 'GET',
-        url: 'https://api.foursquare.com/v2/venues/search?client_id=R3FGZMI124B1GQBTLAS45EZ5YC0TPT3VI0ZANLTUEROIORER&client_secret=2NM0Z15Z3NYGDGVQSFT2FOUDQZ4PPMB4L3GH3Y4HJKJALHBV&v=20130815&ll=51.499840,-0.5530055&query='+search,
+        url: 'https://api.foursquare.com/v2/venues/search?client_id='+clientId+'&client_secret='+clientSecret+'&v=20130815&ll='+location.longitude+','+location.latitude+'&query='+search,
       }).then(function successCallback(response) {
         console.log(response.data.response.venues)
         $scope.venues=response.data.response.venues;
@@ -47,27 +50,96 @@ $http({
 
 .controller('DiscoverDetailCtrl', function($scope, $stateParams) {
 
+
+  function getVenue(){
+    $http({
+        method: 'GET',
+        url: 'https://api.foursquare.com/v2/venues/search?client_id='+clientId+'&client_secret='+clientSecret+'&v=20130815&ll='+location.longitude+','+location.latitude+'&query='+search,
+      }).then(function successCallback(response) {
+        console.log(response.data.response.venues)
+        $scope.venues=response.data.response.venues;
+      }, function errorCallback(response) {
+        console.log(response)
+      });
+  }
+  getVenue();
+
+
 })
 
 .controller('MotivesCtrl', function($scope, MotivesService) {
 
+  $scope.$on('$ionicView.enter', function(e) {
+  	getMotives();
+  });
+
+  function getMotives(){
+	MotivesService.getMotives()
+	  .then(function(response) {
+	  	console.log(response);
+	  	$scope.motives = response;
+	  })
+  }
+  
+
   $scope.addMotive = function () {
 
   	var motive = {
-  		title:"Tenpin Bowling",
+  		title:"Default name",
+  		description:"",
   		foursquareId:"",
   		location:"",
   	}
 
-	MotivesService.createMotive({
-	  success: function(response) {
+	MotivesService.createMotive(motive)
+	  .then(function(response) {
 	  	console.log(response);
-	  },
-	  error: function(error) {
-	  	console.log(error);
-	  }
-	});    
+	  	getMotives();
+	  })
+
   }
+
+})
+
+.controller('MotivesDetailCtrl', function($scope, $state, $stateParams, MotivesService) {
+
+	$scope.editMode = false;
+	function getMotive(){
+		MotivesService.getMotive($stateParams.motivesId)
+		  .then(function(response) {
+		  	console.log(response);
+		  	$scope.motive = response;
+		  	$scope.motiveData = {
+		  		title:response.attributes.title,
+		  		date:response.attributes.date,
+		  		description:response.attributes.description,
+		  	}
+		  })		
+	}
+	getMotive();
+
+	$scope.toggleEdit = function() {
+		$scope.editMode = !$scope.editMode;
+		var editMode = $scope.editMode;
+
+		if(editMode == false){
+			// save
+			MotivesService.updateMotive($scope.motive, $scope.motiveData)
+			  .then(function(response) {
+			  	console.log(response);
+
+			  })
+		}
+	}
+
+	$scope.deleteMotive = function(){
+		// save
+		MotivesService.deleteMotive($scope.motive)
+		  .then(function(response) {
+		  	console.log(response);
+		  	$state.go("tab.motives");
+		  })
+	}
 
 })
 
--- www/js/services.js
+++ www/js/services.js
@@ -9,12 +9,51 @@ angular.module('starter.services', [])
                   var Motive = Parse.Object.extend("Motives");
                   var motive = new Motive();
                   motive.set("title", _data.title);
+                  motive.set("description", _data.description);
                   motive.set("foursquareId", _data.foursquareId);
                   motive.set("date", _data.date);
                   motive.set("location", _data.location);
                   motive.set("attendees", _data.attendees);
                   // should return a promise
                   return motive.save({});
+
+              },
+
+              updateMotive: function (motive, _data) {
+                  console.log(motive)
+                  console.log(_data)
+                  motive.set("title", _data.title);
+                  motive.set("description", _data.description);
+                  motive.set("foursquareId", _data.foursquareId);
+                  motive.set("date", _data.date);
+                  motive.set("location", _data.location);
+                  motive.set("attendees", _data.attendees);
+                  // should return a promise
+                  return motive.save({});
+
+              },
+
+              deleteMotive: function (motive) {
+
+                  // should return a promise
+                  return motive.destroy({});
+
+              },
+
+              getMotives: function () {
+
+                var Motive = Parse.Object.extend("Motives");
+                var query = new Parse.Query(Motive);
+
+                return query.find({});
+
+              },
+
+              getMotive: function (id) {
+
+                var Motive = Parse.Object.extend("Motives");
+                var query = new Parse.Query(Motive);
+                return query.get(id, {});
 
               },
 
