9f9ed881042f4a57abd8d3347f9bae614a833e56
James Newbury <James Newbury>
2015-12-05 19:30:50 +0000

    discover api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,14 +2,46 @@ angular.module('starter.controllers', [])
 
 .controller('MainCtrl', function($scope) {})
 
-.controller('DiscoverCtrl', function($scope) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('DiscoverCtrl', function($scope,$http) {
+
+  var location = {
+    longitude:"51.499840",
+    latitude:"-0.5530055"
+  }
+
+
+$http({
+  method: 'GET',
+  url: 'https://api.foursquare.com/v2/venues/search?client_id=R3FGZMI124B1GQBTLAS45EZ5YC0TPT3VI0ZANLTUEROIORER&client_secret=2NM0Z15Z3NYGDGVQSFT2FOUDQZ4PPMB4L3GH3Y4HJKJALHBV&v=20130815&ll=51.499840,-0.5530055&query=',
+}).then(function successCallback(response) {
+        console.log(response.data.response.venues)
+        $scope.venues=response.data.response.venues;
+  }, function errorCallback(response) {
+    console.log(response)
+  });
+
+
+
+
+  function getVenues(search){
+    $http({
+        method: 'GET',
+        url: 'https://api.foursquare.com/v2/venues/search?client_id=R3FGZMI124B1GQBTLAS45EZ5YC0TPT3VI0ZANLTUEROIORER&client_secret=2NM0Z15Z3NYGDGVQSFT2FOUDQZ4PPMB4L3GH3Y4HJKJALHBV&v=20130815&ll=51.499840,-0.5530055&query='+search,
+      }).then(function successCallback(response) {
+        console.log(response.data.response.venues)
+        $scope.venues=response.data.response.venues;
+      }, function errorCallback(response) {
+        console.log(response)
+      });
+  }
+
+
+  $scope.doSearch = function (search) {
+    getVenues(search)
+  }
+
+
+
 
 })
 
